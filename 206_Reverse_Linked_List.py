5c01d8c3bd5d63abef43653b4619376df80f40d0
Cancan Huang <cancan_huang@brown.edu>
2019-07-21 17:13:47 -0400

    add 24, 206

---
+++ 206_Reverse_Linked_List.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Cancan Huang @ 2019-07-21
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        output = head
+        if not head or not head.next:
+            return head
+        head = self.reverseList(head.next)
+        output.next.next = output
+        output.next = None
+        return head
