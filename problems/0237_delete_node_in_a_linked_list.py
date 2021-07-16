5725fa02b807eff1f107362b66fdb9cc9aa55bd9
Cancan <cancan_huang@brown.edu>
2021-07-16 05:54:19 +0000

    check 0237

--- problems/0237_delete_node_in_a_linked_list.py
+++ problems/0237_delete_node_in_a_linked_list.py
@@ -7,11 +7,13 @@
 #         self.val = x
 #         self.next = None
 
+
 class Solution:
     def deleteNode(self, node):
         """
         :type node: ListNode
         :rtype: void Do not return anything, modify node in-place instead.
         """
+        # only need to change the value to next node and skip next node.
         node.val = node.next.val
         node.next = node.next.next
