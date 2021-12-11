20044d5d1a0894ed1a1b76304c956824f67e065f
Cancan Huang <cancan_huang@brown.edu>
2021-12-11 14:24:11 -0500

    add: solution.py template

--- solutions/0000_test.py
+++ solutions/0000_test.py
@@ -6,23 +6,9 @@ from utils import *
 
 
 class Solution:
-    def minWindow(self, s, t):
-        minwindow = ""
-        count = t.counter()
-        p1, p2 = 0, 0
-        for p2 in range(len(s)):
-            if count[s[p2]] >= 0:
-                pass 
-            while cnt == len(t):
-                if (r - l + 1 < min_size):
-                    min_l = 
-                if 
-            minwindow = s[p1:p2]
-        return minwindow
+    def solution(self, input):
+        pass
 
 
 if __name__ == "__main__":
-    # 0076
-    s = "ADOBECODEBANC"
-    t = "ABC"
-    print(Solution().minWindow(s, t))
+    print(Solution().solution(input))
